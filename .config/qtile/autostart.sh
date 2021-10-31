c11d9516ff57380403d7aee69ad4d0ddb211ec57
Dani-Tb <danitorrens@gmail.com>
2021-10-31 23:54:24 +0100

    Improve qtile config

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,7 +5,7 @@
 xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
 
 lxsession & 
-picom &
+picom --xrender-sync-fence &
 volumeicon &
 nm-applet &
 
--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -3,6 +3,6 @@ from libqtile import bar
 from widgets import init_widgets_lite, init_widgets_full
 
 def init_screens():
-    return [Screen(top=bar.Bar(widgets=init_widgets_full(), opacity=1.0, size=20)),
-            Screen(top=bar.Bar(widgets=init_widgets_lite(), opacity=1.0, size=20))]
+    return [Screen(top=bar.Bar(widgets=init_widgets_full(), opacity=1.0, size=24)),
+            Screen(top=bar.Bar(widgets=init_widgets_lite(), opacity=1.0, size=24))]
 
