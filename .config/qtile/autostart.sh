8fc54194b185e2ec812e9079008c2672dc3db86c
Dani-Tb <danitorrens@gmail.com>
2021-11-01 06:38:11 +0100

    fix: force correct DPI as nvidia fails to do it

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,6 +4,9 @@
 # screen pos
 xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
 
+# force DPI as propietary nvidia driver UseEdidDpi fails to detect correct DPI
+xrandr --dpi 96
+
 lxsession & 
 picom --experimental-backends --backend glx --xrender-sync-fence &
 volumeicon &
